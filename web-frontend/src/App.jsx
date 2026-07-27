dc0da9505fea8311c1f30988e0f9d735c451fe39
AlexArguetaNet <alexarguetanet@gmail.com>
2026-07-27 13:38:59 -0400

    Add UI to homepage
    Installed additional Shadcn UI components to the homepage
    to create the base UI. The UI consists of the title, a
    text area, a button, and a character counter. For now,
    the UI is static except for the character counter. All
    components are used in App.jsx.

--- web-frontend/src/App.jsx
+++ web-frontend/src/App.jsx
@@ -1,13 +1,30 @@
 import { Button } from "./components/ui/button"
+import { Textarea } from "./components/ui/textarea"
 import { useState } from "react"
+import { GiSamuraiHelmet } from "react-icons/gi"
 
 function App() {
 
-  const [isClicked, setIsClicked] = useState(false)
+  const [text, setText] = useState("");
 
   return (
     <>
-      <Button variant="secondary" className="bg-emerald-200">El Boton</Button>
+      <div className="mx-auto mt-7 max-w-[700px] p-4 bg-gray-50 shadow-md">
+        <h1 className="flex align-middle text-4xl pb-5">Summarai <GiSamuraiHelmet /></h1>
+
+        <Textarea 
+          className="h-72 resize-none text-xl" 
+          placeholder="Enter your text here" 
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+
+        <div className="flex justify-between py-5">
+          <Button>Summarize</Button>
+          <p>Character count | {text.length}</p>
+        </div>
+
+      </div>
     </>
   )
 }
