8c9d9376f24d568e75a3f44bdf08f27c6355ad3a
AlexArguetaNet <alexarguetanet@gmail.com>
2026-07-30 17:00:25 -0400

    Add API Integration to Web Frontend
    API integration was implemented in the web frontend.
    The new summary.js module contains a function that calls
    the FastAPI backend and requests the three-bullet-point
    summary of text that is passed as an argument. UI components
    in App.jsx have been changed to handle user's entering text
    in in the application to be summarized.
    
    When text longer than 250 characters is entered into the
    text area and the summarize button is clicked, a spinner
    appears on the screen while the request is processing. Once
    a response (without errors) is received, the summarized text
    is formatted into three bullet points. The copy button copies
    the summary to the user's clipboard, while the clear button
    deletes the summary and the original text entered in the text
    area.

--- web-frontend/src/App.jsx
+++ web-frontend/src/App.jsx
@@ -1,15 +1,49 @@
 import { Button } from "./components/ui/button"
 import { Textarea } from "./components/ui/textarea"
+import { Spinner } from "./components/ui/spinner"
 import { useState } from "react"
 import { GiSamuraiHelmet } from "react-icons/gi"
+import { fetchSummary } from "./api/summary"
 
 function App() {
 
   const [text, setText] = useState("");
+  const [summaryArr, setSummaryArr] = useState([]);
+  const [summaryStr, setSummaryStr] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [isCopied, setIsCopied] = useState(false)
+
+  const handleClick = async () => {
+
+    setIsLoading(true);
+    try {
+      const summary = await fetchSummary(text);
+      console.log(summary);
+      setSummaryArr(summary.summaryArray);
+      setSummaryStr(summary.summaryString);
+
+    } catch (error) {
+      alert("An error occurred");
+    }
+
+    setIsLoading(false);
+
+  }
+
+  const handleClear = () => {
+    setSummaryArr("");
+    setText("");
+    isCopied(false);
+  }
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(summaryStr);
+    setIsCopied(true);
+  }
 
   return (
     <>
-      <div className="mx-auto mt-7 max-w-[700px] p-4 bg-gray-50 shadow-md">
+      <div className="mx-auto my-7 max-w-[700px] p-4 bg-gray-50 shadow-md">
         <h1 className="flex align-middle text-4xl pb-5">Summarai <GiSamuraiHelmet /></h1>
 
         <Textarea 
@@ -20,8 +54,32 @@ function App() {
         />
 
         <div className="flex justify-between py-5">
-          <Button>Summarize</Button>
+          <Button onClick={handleClick} >Summarize</Button>
           <p>Character count | {text.length}</p>
+        </div>
+
+        <div className="flex justify-center">
+          {
+            isLoading && <Spinner className="size-7" />
+          }
+          {
+            summaryArr.length > 0 && 
+              <div className="">
+                <div className="px-10">
+                  {
+                    summaryArr.map((elem, index) => (
+                      <li key={index}>{elem}</li>
+                    ))
+                  }
+                </div>
+                <div className="flex justify-end mt-5">
+                  
+                  {isCopied && <p>Copied!</p>}
+                  <Button onClick={handleCopy}>Copy</Button>
+                  <Button onClick={handleClear}>Clear</Button>
+                </div>
+              </div>
+          }
         </div>
 
       </div>
---
+++ web-frontend/src/api/summary.js
@@ -0,0 +1,38 @@
+import axios from "axios"
+
+const API_PREFIX = import.meta.env.VITE_ENDPOINT_PREFIX;
+
+export const fetchSummary = async (text) => {
+
+    try {
+        const res = await axios.post(`${API_PREFIX}/summarize`, { text });
+
+        // Format summary string
+        const summaryString = res.data.summary
+            .split('\n')
+            .filter(line => line.trim().startsWith('*'))
+            .join(' ');
+
+        // Format summary into an array
+        const summaryArray = res.data.summary
+            .split(/(?=\*)/)                          // Split before every '*'
+            .filter(str => str.startsWith('*'))       // Discard anything without '*'
+            .map(str => str.replace('*', '').trim()); // Strip the '*' and trim whitespace
+        
+            return {
+                summaryArray,
+                summaryString
+            };
+                
+    } catch (error) {
+        console.log("Failed to fetch summary", error);
+        
+        throw new Error(
+            error.response?.data?.detail ||
+            error.message ||
+            "An error occurred while getting the summary."
+        )
+        
+    }
+    
+}
