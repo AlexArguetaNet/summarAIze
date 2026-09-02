0903f24b57c138c5f326b2031f12e9a2f6fa126d
AlexArguetaNet <alexarguetanet@gmail.com>
2026-09-02 17:05:45 -0400

    Add url and paragraph text components to the UI with a switch

--- web-frontend/src/components/Home.jsx
+++ web-frontend/src/components/Home.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { useState } from 'react'
+import { Textarea } from './ui/textarea'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 function Home() {
+
+  const [usingUrl, setUsingUrl] = useState(true);
+
   return (
-    <div className="bg-blue-100 border rounded-4xl h-[500px] mx-auto w-[60%] max-w-[900px]">
+
+    <div className="bg-[var(--glass-bg-light)] backdrop-blur-xl border border-[var(--glass-border-light)] rounded-4xl h-[500px] mx-auto w-[70%] max-w-[900px] flex justify-center">
+      <div className='w-full m-5'>
+
+        <div className='flex justify-end'>
+          <Tabs defaultValue="url" className="">
+            <TabsList>
+              <TabsTrigger onClick={() => setUsingUrl(true)} value="url">URL</TabsTrigger>
+              <TabsTrigger onClick={() => setUsingUrl(false)} value="paragraph">Paragraph</TabsTrigger>
+            </TabsList>
+          </Tabs>
+        </div>
+
+        {
+          usingUrl ? <Textarea className="text-white! text-xl! placeholder:text-white/70! mt-5 resize-none border border-white/50 focus-visible:border-[#ffffff] focus-visible:ring-0" placeholder="Enter your url here ..." /> 
+                    : <Textarea className="text-white! text-xl! placeholder:text-white/70! mt-5 h-75 resize-none border border-white/50 focus-visible:border-[#ffffff] focus-visible:ring-0" placeholder="Enter your text here ..." />
+        }
+
+      </div>
+
+
 
     </div>
   )
