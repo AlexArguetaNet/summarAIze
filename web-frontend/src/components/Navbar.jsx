d2658782cd8fdb9efb2d968adaa9f58dc0620b15
AlexArguetaNet <alexarguetanet@gmail.com>
2026-09-18 15:59:45 -0400

    Changed the title font

--- web-frontend/src/components/Navbar.jsx
+++ web-frontend/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
     <div className='p-5 mb-20'>
 
         <div className='mx-auto flex justify-between w-[80%] max-w-[1100px] text-custom'>
-            <h1 className='text-4xl'>SummarAIze</h1>
+            <h1 className='text-4xl font-title'>SummarAIze</h1>
             <a href="https://github.com/AlexArguetaNet/summarAIze" className='flex items-center text-xl'>GitHub <FaGithub className='ml-2 text-2xl' /></a>
         </div>
       
